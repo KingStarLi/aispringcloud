4e3e86c69db55b42f0b681dec558dbb847e3d2f1
KingStarLi <1127882911@qq.com>
2020-10-16 16:03:33 +0800

    Initial commit

---
+++ hystrix/src/main/java/com/southwind/feign/FeignProviderClient.java
@@ -0,0 +1,18 @@
+package com.southwind.feign;
+
+import com.southwind.entity.Student;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
+
+@FeignClient(value = "provider")
+@Component
+public interface FeignProviderClient {
+
+    @GetMapping("/student/findAll")
+    public Collection<Student> findAll();
+    @GetMapping("/student/index")
+    public String index();
+}
